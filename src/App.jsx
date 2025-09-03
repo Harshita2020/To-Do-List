c73982760b47b896142b3f3f561a1173dd174fa2
Harshita2020 <harshitaautocad@gmail.com>
2025-09-04 03:01:10 +0530

    Fix states, Code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,23 @@ import Tasks from "./components/Tasks";
 
 function App() {
   const [task, setTask] = useState("");
-  const [tasks, setTasks] = useState(["jkfn", "lkdn"]);
+  const [tasks, setTasks] = useState(["Implement LocalStorage in Todo list app", "Work on context API", "Figure out AutoMailer"]);
   const [isUpdateSelected, setIsUpdateSelected] = useState({
     id: "",
     text: "",
     status: false,
   });
 
-  // useEffect((e)=> {
-  //   handleKeyDown(e)
-  // },[task])
-
   const handleKeyDown = (e) => {
-    // console.log(e)
     if (task.trim() !== "" && e.key == "Enter") {
-      // console.log(e.key)
       setTasks((prev) => [...prev, task]);
       setTask("");
     }
   };
   const handleChange = (e) => {
-    // console.log("event", e.target.value)
     setTask(e.target.value);
   };
 
-  console.log("Length - ", tasks);
   return (
     <>
       <div className="my-8 mx-auto flex justify-center text-center w-1/2 ">
